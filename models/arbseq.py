dc33f8ed155540a3cb5be5dac9cc4c993847c402
Ulzee An <ul1994@gmail.com>
2018-09-28 19:43:50 +0000

    is it a good idea to focus on each module?

--- models/arbseq.py
+++ models/arbseq.py
@@ -14,68 +14,82 @@ class SpawnNet(nn.Module):
 
 	# Spawns arbitrary len seq:
 
-	def __init__(self, hsize, zsize=20, states=128):
+	def __init__(self, hsize, zsize=20, lstm_size=128):
 		super(SpawnNet, self).__init__()
-		self.fcs = [
-			# each observation is noise itself
-			nn.Linear(zsize, 128),
-			nn.Linear(128, 512),
-			nn.Linear(512, 512),
-			nn.Linear(512, states),
-		]
 
-		self.hout = [
-			nn.Linear(states, 128),
-			nn.Linear(128, 128),
-			nn.Linear(128, hsize)
-		]
+		self.fcs = nn.ModuleList([
+			# In arbseq case, each observation is noise alone
+			nn.Linear(zsize, 256),
+			nn.Linear(256, 512),
+			nn.Linear(512, 1024),
+			nn.Linear(1024, 1024),
+			nn.Linear(1024, lstm_size),
+		])
 
-		self.lstm = nn.LSTM(states, states)
-		self.lstm_states = states
+		self.hout = nn.ModuleList([
+			nn.Linear(lstm_size + zsize, 512),
+			nn.Linear(512, 1024),
+			nn.Linear(1024, 1024),
+			nn.Linear(1024, hsize)
+		])
+
+		self.lstm = nn.LSTM(lstm_size, lstm_size)
+		self.lstm_size = lstm_size
 		self.noise_size = zsize
 
-	def forward(self, noise, state):
+	def forward(self, noise_sample, noise_iter, state):
 		# state enocdes the progress in sequence
 		# input = torch.cat([noise])
-		input = noise
+		input = noise_sample
 		x = F.relu(self.fcs[0](input))
 		x = F.relu(self.fcs[1](x))
 		x = F.relu(self.fcs[2](x))
 		x = F.relu(self.fcs[3](x))
+		x = F.relu(self.fcs[4](x))
 
 		# output for this timestep: output at T , cell state
 		# The axes semantics are (num_layers, minibatch_size, state_dim)
 		t_output, state = self.lstm(x, state) # TODO: reassign hidden
 
+		# t_output = F.relu(t_output)
+
+		x = torch.cat([t_output, noise_iter], -1)
+
 		# h_v itself can be a termination character, iwc inference stops
-		h_v = F.relu(self.hout[0](t_output))
-		h_v = F.relu(self.hout[1](h_v))
-		h_v = torch.tanh(self.hout[2](h_v))
+		x = F.relu(self.hout[0](x))
+		x = F.relu(self.hout[1](x))
+		x = F.relu(self.hout[2](x))
+		x = torch.tanh(self.hout[3](x))
 
-		return h_v, state
+		return x, state
 
-	def init_state(self):
+	def init_state(self, device='cpu'):
 		# These are placeholders for LSTM cell state
 		return (
-			torch.zeros(1, 1, self.lstm_states),
-			torch.zeros(1, 1, self.lstm_states))
+			torch.zeros(1, 1, self.lstm_size).to(device),
+			torch.zeros(1, 1, self.lstm_size).to(device))
+		# return torch.zeros(1, 1, self.lstm_size).to(device)
 
-	def init_noise(self):
-		return Variable(torch.rand((1, 1, self.noise_size)))
+	def init_noise(self, size=None, device='cpu'):
+		# return Variable(torch.rand((1, 1, self.noise_size)).to(device))
+		return Variable(
+			torch.FloatTensor(1, 1, self.noise_size if size is None else size) \
+				.uniform_(-1, 1)).to(device)
 
 
 class ReadNet(nn.Module):
 	def __init__(self, hsize):
 		super(ReadNet, self).__init__()
-		self.fcs = [
+		self.fcs = nn.ModuleList([
 			nn.Linear(hsize, 128),
 			nn.Linear(128, 128),
 			nn.Linear(128, 128),
 			nn.Linear(128, hsize)
-		]
+		])
+		self.hsize = hsize
 
-	def forward(self, seqembed):
-		input = seqembed
+	def forward(self, input):
+		input = input.view(self.hsize)
 		x = F.relu(self.fcs[0](input))
 		x = F.relu(self.fcs[1](x))
 		x = F.relu(self.fcs[2](x))
@@ -86,12 +100,12 @@ class ReadNet(nn.Module):
 class DiscrimNet(nn.Module):
 	def __init__(self, hsize):
 		super(DiscrimNet, self).__init__()
-		self.fcs = [
+		self.fcs = nn.ModuleList([
 			nn.Linear(hsize, 2048),
 			nn.Linear(2048, 2048),
 			nn.Linear(2048, 2048),
 			nn.Linear(2048, 1),
-		]
+		])
 
 	def forward(self, x):
 		x = F.relu(self.fcs[0](x))
@@ -99,4 +113,5 @@ class DiscrimNet(nn.Module):
 		x = F.relu(self.fcs[2](x))
 		x = self.fcs[3](x)
 
-		return nn.Sigmoid()(x)
+		# return nn.Sigmoid()(x)
+		return x
--- train_arbseq.py
+++ train_arbseq.py
@@ -19,29 +19,27 @@ from torchvision import datasets, transforms
 from random import randint
 from models.arbseq import SpawnNet, ReadNet, DiscrimNet
 from torch.autograd import Variable
+import numpy as np
 
-def timeSince(since):
-	now = time.time()
-	s = now - since
-	m = math.floor(s / 60)
-	s -= m * 60
-	return '%dm %ds' % (m, s)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-HSIZE = 5
-ZSIZE = 50
-ITERS = 1
+# HSIZE = 5
+ZSIZE = 100
+# ITERS = 1
+LSTM_SIZE=512
 BSIZE = 64
-# LR = 5e-4
-LR = 5e-3
-criterion = nn.NLLLoss()
+LR = 5e-5
 all_letters = 'abcdefghijklmnopqrstuvwxyz'
-n_letters = len(all_letters) + 1 # Plus EOS marker
-adversarial_loss = torch.nn.BCELoss()
-MAXLEN = 10
+# n_letters = len(all_letters) + 1 # Plus EOS marker
+n_letters = len(all_letters)
+# adversarial_loss = torch.nn.BCELoss().to(device)
+adversarial_loss = torch.nn.BCEWithLogitsLoss().to(device)
+MAXLEN = 4
 
-spawn = SpawnNet(hsize=n_letters, zsize=20)
-readout = ReadNet(hsize=n_letters)
-discrim = DiscrimNet(hsize=n_letters)
+
+spawn = SpawnNet(hsize=n_letters, zsize=ZSIZE, lstm_size=LSTM_SIZE).to(device)
+readout = ReadNet(hsize=n_letters).to(device)
+discrim = DiscrimNet(hsize=n_letters).to(device)
 
 gen_opt = optim.Adam([
 	{ 'params': spawn.parameters() },
@@ -63,7 +61,7 @@ def toTensor(string):
 		tensor = torch.zeros(n_letters)
 		letter = string[li]
 		tensor[all_letters.find(letter)] = 1
-		tensor = Variable(tensor)
+		tensor = Variable(tensor).to(device)
 
 		sample.append(tensor)
 	return sample
@@ -71,17 +69,28 @@ def toTensor(string):
 def drawSample(verbose=False):
 	# strlen = randint(5, MAXLEN)
 	strlen = MAXLEN
-	hat = 'xyz'
+	hat1 = 'cd'
 
 	line = ''
-	for ii in range(strlen):
-		line += hat[randint(0, len(hat)-1)]
+	for ii in range(strlen-1):
+		line += hat1[randint(0, len(hat1)-1)]
+	line += 'x'
 
 	if verbose: print(line)
 
 	tensor = toTensor(line)
 	return tensor, line
 
+def toString(embedding):
+	strlen = len(embedding) # num chars
+
+	line = ''
+	for ii in range(strlen):
+		vect = embedding[ii]
+		vect = vect.detach().cpu().numpy()
+		ind = np.argmax(vect)
+		line += all_letters[ind]
+	return line
 # input is string in embedded form per char
 # target is input shifted by one (string[1:]) + EOS termination char
 # LSTM will be trained to guess the next char
@@ -94,18 +103,16 @@ total_loss = 0 # Reset every plot_every iters
 start = time.time()
 
 bhalf = BSIZE // 2
-real_labels = Variable(torch.ones(bhalf, 1), requires_grad=False)
-fake_labels = Variable(torch.zeros(bhalf, 1), requires_grad=False)
+real_labels = Variable(torch.ones(bhalf, 1), requires_grad=False).to(device)
+fake_labels = Variable(torch.zeros(bhalf, 1), requires_grad=False).to(device)
 
 def get_readout(sample):
 	# sample: h_1, h_2, ..., h_T
 	slen = len(sample)
 
-	rsum = Variable(torch.zeros(n_letters))
-	# R_G = readout(sample[0].view(n_letters))
-	for ii in range(1, slen):
-		r_t = readout(sample[ii].view(n_letters))
-		# print(r_t.size(), rsum.size())
+	rsum = Variable(torch.zeros(n_letters)).to(device)
+	for ii in range(slen):
+		r_t = readout(sample[ii])
 		rsum.add_(r_t)
 
 	return rsum.unsqueeze(0)
@@ -113,22 +120,25 @@ def get_readout(sample):
 for iter in range(1, n_iters + 1):
 	real_readouts = []
 	for _ in range(bhalf):
-		sample, _ = drawSample(verbose=False)
+		sample, as_string = drawSample(verbose=False)
 		R_G = get_readout(sample)
 		real_readouts.append(R_G)
 
-	spawn.zero_grad()
 	fake_readouts = []
 	for _ in range(bhalf):
 		# target_line_tensor.unsqueeze_(-1)
-		state = spawn.init_state()
-		# type_noise = spawn.init_noise() # dist of types of strings
+		state = spawn.init_state(device=device)
+
+		# sample level distribution
+		noise_sample = spawn.init_noise(device=device)
 
 		fake_hs = []
-		for _ in range(MAXLEN):
-			noise = spawn.init_noise() # dist of char in string
+		for lii in range(MAXLEN):
+			 # iteration level distribution
+			noise_iter = spawn.init_noise(device=device)
 			# every step, (noise, state) are used to generate an embedding
-			h_t, state = spawn(noise, state)
+			h_t, state = spawn(noise_sample, noise_iter, state)
+			# h_t, state = spawn(noise_iter, state)
 			fake_hs.append(h_t)
 
 		# fake_sample = torch.cat(fake_hs, 0)
@@ -137,34 +147,42 @@ for iter in range(1, n_iters + 1):
 
 	# print(fake_readouts[0])
 	# print(real_readouts[0])
-	fake_readouts = torch.cat(fake_readouts, 0)
-	real_readouts = torch.cat(real_readouts, 0)
+	fake_readouts = torch.cat(fake_readouts, 0).to(device)
+	real_readouts = torch.cat(real_readouts, 0).to(device)
 
 	# print(real_readouts[0])
 	# print(fake_readouts[0])
 
-	readout.zero_grad()
-	if iter > 1000:
+	# if iter > 50 and iter % 3 == 0:
+	# if iter > 100 and iter % 10 == 0:
+
+	if iter % 10  == 0:
+		print('[%d] Sample: %s  vs  %s' % (
+			iter,
+			toString(fake_hs),
+			as_string))
+
+	# -- Generator Training --
+	if iter % 100 > 50:
 		spawn.zero_grad()
-		# -- Generator Training --
 		gen_loss = adversarial_loss(discrim(fake_readouts), real_labels)
 		gen_loss.backward(retain_graph=True)
 		gen_opt.step()
-		print('Generate/L: %.5f  (fooling: lower is better)' % (
-			gen_loss.item(),))
+		print('[%d] Generate/L: %.5f      (fooling: lower is better)' % (
+			iter, gen_loss.item(),))
+		continue
 
 	# -- Discrimination Training --
 	# NOTE: Readout gradients carry over to discriminiator training
-	discrim.zero_grad()
 
+	readout.zero_grad()
+	discrim.zero_grad()
 	real_loss = adversarial_loss(discrim(real_readouts), real_labels)
 	fake_loss = adversarial_loss(discrim(fake_readouts.detach()), fake_labels)
 	discrim_loss = (real_loss + fake_loss) / 2
 	discrim_loss.backward()
 	discrim_opt.step()
 
-	print('Discrim/L : %.5f  (discriminate: will increase)' % (
-		discrim_loss.item(),))
+	print('[%d] Discrim/L : %.5f      (discrim: high if generator fools well)' % (
+		iter, discrim_loss.item(),))
 
-
-	# break
